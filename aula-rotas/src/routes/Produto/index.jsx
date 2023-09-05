931399634e467872567d79fad5cfeab0454842a7
VikitorXD <rm551886@fiap.com.br>
2023-09-04 23:16:47 -0300

    alterando valores da lista.js

--- aula-rotas/src/routes/Produto/index.jsx
+++ aula-rotas/src/routes/Produto/index.jsx
@@ -1,8 +1,18 @@
+import { Link } from 'react-router-dom'
+import { listaProdutos } from '../../componentes/listaPrudutos'
+
 export default function Produto(){
 
     return(
         <main>
             <h1>Produtos</h1>
+            {listaProdutos.map(prod =>(
+                <div key={prod.id}>
+                    <Link to={`/produto/editar/${prod.id}`}>
+                        Editar o Produto: {prod.name}
+                    </Link>
+                </div>
+            ))}
         </main>
     )
 }
